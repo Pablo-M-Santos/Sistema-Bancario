98446535daa1bdc13c8d5efca8813d2a0a0a2a95
Pablo Moreira Santos <Pablomoreirasantos.hp@gmail.com>
2024-03-12 20:11:40 -0300

    Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,40 +1,27 @@
-class Cliente {
-    private String nome;
-    private Banco banco;
-    private Conta conta;
+ class Cliente {
+    private String nome; // Nome do cliente
+    private Banco banco; // Banco em que o cliente possui conta
+    private Conta conta; // Conta do cliente no banco
 
+    // Construtor que recebe o nome do cliente, o banco e a conta
     public Cliente(String nome, Banco banco, Conta conta) {
         this.nome = nome;
         this.banco = banco;
         this.conta = conta;
-        this.conta.setCliente(this);
     }
 
+    // Método para obter o nome do cliente
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
+    // Método para obter o banco do cliente
     public Banco getBanco() {
         return banco;
     }
 
-    public void setBanco(Banco banco) {
-        this.banco = banco;
-    }
-
+    // Método para obter a conta do cliente
     public Conta getConta() {
         return conta;
-    }
-
-    public void setConta(Conta conta) {
-        this.conta = conta;
-    }
-    
-    public String getBancoNome() {
-        return banco.getNome();
     }
 }
