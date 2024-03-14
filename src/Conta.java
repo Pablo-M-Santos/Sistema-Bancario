0455c8ce2e7be6c58c56196396e2dc08df1e1ce3
Pablo Moreira Santos <Pablomoreirasantos.hp@gmail.com>
2024-03-14 08:01:09 -0300

    Conta.java

--- src/Conta.java
+++ src/Conta.java
@@ -1,25 +1,27 @@
-class Banco {
-    private String nome;
-    private List<Cliente> clientes;
+class Conta {
+    private int numero; // Número da conta
+    private String agencia; // Agência da conta
+    private double saldo; // Saldo da conta
 
-    public Banco(String nome) {
-        this.clientes = new ArrayList<>();
-        this.nome = nome;
+    // Construtor que recebe o número da conta e a agência
+    public Conta(int numero, String agencia) {
+        this.numero = numero; // Define o número da conta
+        this.agencia = agencia; // Define a agência da conta
+        this.saldo = 0.0; // Inicializa o saldo da conta como zero
     }
 
-    public void adicionarCliente(Cliente cliente) {
-        clientes.add(cliente);
+    // Método para creditar um valor na conta
+    public void creditar(double valor) {
+        saldo += valor; // Incrementa o saldo da conta pelo valor creditado
     }
 
-    public List<Cliente> getClientes() {
-        return clientes;
+    // Método para obter o saldo da conta
+    public double getSaldo() {
+        return saldo; // Retorna o saldo atual da conta
     }
 
-    public void setClientes(List<Cliente> clientes) {
-        this.clientes = clientes;
-    }
-
-    public String getNome() {
-        return nome;
+    // Método para obter a agência da conta
+    public String getAgencia() {
+        return agencia; // Retorna a agência da conta
     }
 }
