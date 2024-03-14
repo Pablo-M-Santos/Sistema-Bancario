9a240488bc562599a23a9f63666bddd3fd66d57c
Pablo Moreira Santos <Pablomoreirasantos.hp@gmail.com>
2024-03-14 08:01:53 -0300

    Banco.java

--- src/Banco.java
+++ src/Banco.java
@@ -1,25 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Classe que representa um banco
 class Banco {
-    private String nome;
-    private List<Cliente> clientes;
+    private String nome; // Nome do banco
+    private List<Cliente> clientes; // Lista de clientes do banco
 
+    // Construtor que recebe o nome do banco e inicializa a lista de clientes
     public Banco(String nome) {
-        this.clientes = new ArrayList<>();
-        this.nome = nome;
+        this.nome = nome; // Define o nome do banco
+        this.clientes = new ArrayList<>(); // Inicializa a lista de clientes como um ArrayList vazio
     }
 
+    // Método para adicionar um cliente à lista de clientes do banco
     public void adicionarCliente(Cliente cliente) {
-        clientes.add(cliente);
+        clientes.add(cliente); // Adiciona o cliente à lista de clientes
     }
 
+    // Método para obter a lista de clientes do banco
     public List<Cliente> getClientes() {
-        return clientes;
+        return clientes; // Retorna a lista de clientes do banco
     }
 
-    public void setClientes(List<Cliente> clientes) {
-        this.clientes = clientes;
-    }
-
+    // Método para obter o nome do banco
     public String getNome() {
-        return nome;
+        return nome; // Retorna o nome do banco
     }
 }
